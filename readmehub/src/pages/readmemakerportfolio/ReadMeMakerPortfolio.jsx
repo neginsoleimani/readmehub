84df63dbfce18915b9500a1c3971e5ee7bf76ac8
Negin Soleiamni <negin.kdk1@gmail.com>
2023-02-08 15:48:00 +0330

    Add Navbar update homepage

--- readmehub/src/pages/readmemakerportfolio/ReadMeMakerPortfolio.jsx
+++ readmehub/src/pages/readmemakerportfolio/ReadMeMakerPortfolio.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// Components
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+
+
 export default function ReadMeMakerPortfolio() {
   return (
+    <>
+    <Navbar/>
     <div className="container">
       <div className="section">
         <h1>ReadMe Maker as portfolio</h1>
         <br />
         <br />
         <br />
-        <Link to="/" className="primary-outline-btn">
+        <Link to="/" className="secondary-outline-btn" >
           Back to HomePage
         </Link>
       </div>
     </div>
+    <Footer/>
+    </>
   );
 }
--- readmehub/src/pages/readmemakerproject/ReadMeMakerProject.jsx
+++ readmehub/src/pages/readmemakerproject/ReadMeMakerProject.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+// Components
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+
 export default function ReadMeMakerProject() {
   return (
+    <>
+    <Navbar/>
     <div className="container">
       <div className="section">
         <h1>ReadMe Maker for my Project</h1>
         <br />
         <br />
         <br />
-        <Link to="/" className="primary-outline-btn">
+        <Link to="/" className="secondary-outline-btn">
           Back to Homepage
         </Link>
       </div>
     </div>
+    <Footer/>
+    </>
   );
 }
